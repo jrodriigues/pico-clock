7746de520ca3851716f4096659f3f004486c7dbc
Joao Rodrigues <jrodriiguespro@gmail.com>
2023-02-19 22:13:17 +0000

    button1 enabled

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import RGB1602
 from random import randint
 
 lcd=RGB1602.RGB1602(16,2)
+sound = Pin(13, Pin.OUT)
+button1 = Pin(12, Pin.IN) # Changes background color
 
 # set start time : {hour1}{hour2}:{minu1}{minu2}:{sec}
 hour1 = 2
-hour2 = 3
+hour2 = 1
 minu1 = 5
-minu2 = 9
-sec = 5
+minu2 = 5
+sec = 0
 
 # set start date : {dd}/{mm}/{yyyy}
-day = 19
+day = 20
 month = 2
 year = 2023
 
@@ -25,6 +27,8 @@ colors = [randint(0,255),randint(0,255),randint(0,255)]
 lcd.clear()
 
 while True:
+    sound.value(0)
+    
     # Condition that prevents seconds from going beyond 59
     if sec == 6:
         sec = 0
@@ -33,8 +37,7 @@ while True:
     # Condition that prevents minutes from going beyond 59
     if minu2 == 10:
         minu2 = 0
-        minu1 += 1
-        
+        minu1 += 1 
     if minu1 == 6:
         minu1 = 0
         minu2 = 0
@@ -43,7 +46,10 @@ while True:
     # Condition that prevents hours from going beyond 23
     # and starts a new day which triggers the following events:
     # - RGB color changes to a random number
-    
+    if hour2 == 10:
+        hour2 = 0
+        hour1 += 1
+        
     if hour2 == 4 and hour1 == 2:
         hour2 = 0
         hour1 = 0
@@ -54,8 +60,9 @@ while True:
         colors[0] = randint(0,255)
         colors[1] = randint(0,255)
         colors[2] = randint(0,255)
-            
-    lcd.setRGB(colors[0],colors[1],colors[2])
+    
+    # Adjust background color and set cursor ready for date printout
+    #lcd.setRGB(colors[0],colors[1],colors[2])
     lcd.setCursor(3,1)    
     
     if day < 10 and month < 10:
@@ -71,8 +78,14 @@ while True:
         display = f"{hour1}{hour2}:{minu1}{minu2}:{sec}{s}"
         lcd.setCursor(4,0)
         lcd.printout(display)
+        
+        if button1.value() == 0:
+            lcd.setRGB(randint(0,255),randint(0,255),randint(0,255))
+            
         time.sleep(1)
     sec += 1
     
     
+
+
 
